de37350f55d907ad44698c75d084be84b8f3bd92
Илья <@DESKTOP-ALQEF61>
2021-10-13 19:54:12 +0300

    Adding a Dapper repo, modifying the controller for appsettings

--- ASP_OLX/Controllers/HomeController.cs
+++ ASP_OLX/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using ASP_OLX.Models;
+using DapperDll;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,11 +13,19 @@ namespace ASP_OLX.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
         {
-            _logger = logger;
+            _configuration = configuration;
+            ProductSingleton.Init(_configuration.GetConnectionString("DefaultConnection"));
+            ImageSingleton.Init(_configuration.GetConnectionString("DefaultConnection"));
+            CategorySingleton.Init(_configuration.GetConnectionString("DefaultConnection"));
         }
 
         public IActionResult Index()
